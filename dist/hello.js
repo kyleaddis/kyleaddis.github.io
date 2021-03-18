2e28de660088a38d5a6ae457b6b01b94762a9f8b
kyleaddis <addisk22@gmail.com>
2021-03-17 21:59:54 -0500

    - Finished terminal

--- dist/hello.js
+++ dist/hello.js
@@ -1,2 +1,14 @@
 console.log('Hello World!');
+function sleep(ms) {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+}
+function terminal_select() {
+    const select = document.getElementById('term-select');
+    const value = select.options[select.selectedIndex].value;
+    const loading = document.getElementById('term-loading');
+    if (loading) {
+        loading.style.display = 'block';
+    }
+    sleep(1000).then(() => (window.location.href = 'http://127.0.0.1:5500/' + value + '.html'));
+}
 //# sourceMappingURL=hello.js.map
