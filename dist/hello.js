648f78f7617b579471f4954f3722a96d6153bae9
kyleaddis <addisk22@gmail.com>
2021-03-18 20:31:47 -0500

    -polished the terminal

--- dist/hello.js
+++ dist/hello.js
@@ -6,9 +6,21 @@ function terminal_select() {
     const select = document.getElementById('term-select');
     const value = select.options[select.selectedIndex].value;
     const loading = document.getElementById('term-loading');
+    const blinky = document.getElementById('blinking-cursor');
     if (loading) {
         loading.style.display = 'block';
     }
-    sleep(1000).then(() => (window.location.href = 'http://127.0.0.1:5500/' + value + '.html'));
+    if (blinky) {
+        blinky.style.display = 'none';
+    }
+    sleep(1000).then(() => {
+        if (loading) {
+            loading.style.display = 'none';
+        }
+        if (blinky) {
+            blinky.style.display = 'block';
+        }
+        window.location.href = 'http://127.0.0.1:5500/' + value + '.html';
+    });
 }
 //# sourceMappingURL=hello.js.map
